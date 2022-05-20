6e36e2497d18634d76be34de44e7a64129ef751d
jan-zelaznog <jan.zelaznog@gmail.com>
2022-05-20 18:33:08 -0500

    preparación para hacer el ejercicio en equipos

--- Gaga/PictViewController.swift
+++ Gaga/PictViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 class PictViewController: UIViewController {
     var item:Item?
     var imageView = UIImageView()
+    var a_i = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.frame = self.view.frame.insetBy(dx: 20, dy: 20)
+        imageView.contentMode = .scaleAspectFit
         self.view.addSubview(imageView)
+        a_i.style = .large
+        a_i.color = .red
+        a_i.hidesWhenStopped = true
+        a_i.center = self.view.center
+        self.view.addSubview(a_i)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -26,7 +33,30 @@ class PictViewController: UIViewController {
                                 a. se puede obtener directamente (main thread)
                                 b. se implementa una tarea con la clase URLSession
         */
+        // comprobar si existe la imagen local
+        
+        
+        // TODO: Implementa la descarga con URLSession
+        // TODO: Validar la conexión a Internet...
         if let url = URL(string:DataManager.instance.baseURL + "/" + lItem.pict) {
+            let request = URLRequest(url: url)
+            let sesion = URLSession.shared
+            let tarea = sesion.dataTask(with: request) { bytes, response, error in
+                if error != nil {
+                    print ("ocurrio un error \(error!.localizedDescription)")
+                }
+                else {
+                    let image = UIImage(data: bytes!)
+                    self.guardaImagen(bytes!, lItem.pict)
+                    DispatchQueue.main.async {
+                        self.imageView.image = image
+                        self.a_i.stopAnimating()
+                    }
+                }
+            }
+            a_i.startAnimating()
+            tarea.resume()
+            /* ESTO ES PARA DESCARGAR DIRECTAMENTE
              do {
                  let bytes = try Data(contentsOf: url)
                  let image = UIImage(data: bytes)
@@ -34,10 +64,33 @@ class PictViewController: UIViewController {
              }
              catch {
                  print ("ocurrio un error \(error.localizedDescription)")
-             }
+             }*/
         }
     }
     
+    func cargaImagenLocal (_ nombre:String) -> Bool {
+        // 1. Obtener la ruta a documents
+        
+        // 2. comprobar si existe el archivo
+        // 2.a. el objeto URL tiene una propiedad path....
+        // FileManager.default.fileExists(atPath: <#T##String#>) regresa true o false
+        
+        // 3. si el archivo existe, lo cargamos en el imageview y se regresa true
+        
+        // 4. si el archivo no existe se regresa false.
+    }
+    
+    func guardaImagen(_ bytes:Data, _ nombre:String) {
+        // 1. Obtenemos la ubicacion de la carpeta de documents
+        let urlAdocs = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let urlAlArchivo = urlAdocs.appendingPathComponent(nombre)
+        do {
+            try bytes.write(to: urlAlArchivo)
+        }
+        catch {
+            print ("no se puede salvar la imagen \(error.localizedDescription)")
+        }
+    }
     /*
     // MARK: - Navigation
 
